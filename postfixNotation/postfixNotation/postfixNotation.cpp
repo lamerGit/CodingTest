af3b464054de740f3e041c5f19458e8c644de7b9
LeeJuYeol <37298646+lamerGit@users.noreply.github.com>
2022-10-25 19:23:46 +0900

    후위표기식

---
+++ postfixNotation/postfixNotation/postfixNotation.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+//후위 표기식
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	string s;
+	stack<char> op;
+	cin >> s;
+
+	for (auto i : s)
+	{
+		if (i >= 'A' && i <= 'Z')
+		{
+			cout << i;
+		}
+		else
+		{
+			if (i == '(')
+			{
+				op.push(i);
+			}
+			else if (i == '*' || i == '/')
+			{
+				while (!op.empty() && (op.top() == '*' || op.top() == '/'))
+				{
+					cout << op.top();
+					op.pop();
+				}
+				op.push(i);
+			}
+			else if (i == '+' || i == '-')
+			{
+				while (!op.empty() && op.top() != '(')
+				{
+					cout << op.top();
+					op.pop();
+				}
+				op.push(i);
+			}
+			else if (i == ')')
+			{
+				while (!op.empty() && op.top() != '(')
+				{
+					cout << op.top();
+					op.pop();
+				}
+				op.pop();
+			}
+		}
+
+	}
+	while (!op.empty())
+	{
+		cout << op.top();
+		op.pop();
+	}
+
+
+}
