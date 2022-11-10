b720648575c07d4cb95936a5b1d962251293de78
LeeJuYeol <37298646+lamerGit@users.noreply.github.com>
2022-11-10 19:17:23 +0900

    쿼드트리

---
+++ QuadTree/QuadTree/QuadTree.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <string>
+#include <ctime>
+#include <map>
+#include <list>
+#include <queue>
+#include <cstring>
+
+
+using namespace std;
+
+//알고 스팟 쿼드트리
+
+//대량의 좌표 데이터를 메모리 안에 압축해 저장하기 위해 사용하는 여러 기법 중 쿼드 트리(quad tree)란 것이 있습니다.주어진 공간을 항상 4개로 분할해 재귀적으로 표현하기 때문에 쿼드 트리라는 이름이 붙었는데, 이의 유명한 사용처 중 하나는 검은 색과 흰 색밖에 없는 흑백 그림을 압축해 표현하는 것입니다.쿼드 트리는 2N × 2N 크기의 흑백 그림을 다음과 같은 과정을 거쳐 문자열로 압축합니다.
+//
+//이 그림의 모든 픽셀이 검은 색일 경우 이 그림의 쿼드 트리 압축 결과는 그림의 크기에 관계없이 b가 됩니다.
+//이 그림의 모든 픽셀이 흰 색일 경우 이 그림의 쿼드 트리 압축 결과는 그림의 크기에 관계없이 w가 됩니다.
+//모든 픽셀이 같은 색이 아니라면, 쿼드 트리는 이 그림을 가로 세로로 각각 2등분해 4개의 조각으로 쪼갠 뒤 각각을 쿼드 트리 압축합니다.이때 전체 그림의 압축 결과는 x(왼쪽 위 부분의 압축 결과)(오른쪽 위 부분의 압축 결과)(왼쪽 아래 부분의 압축 결과)(오른쪽 아래 부분의 압축 결과)가 됩니다.예를 들어 그림(a)의 왼쪽 위 4분면은 xwwwb로 압축됩니다.
+//그림(a)와 그림(b)는 16×16 크기의 예제 그림을 쿼드 트리가 어떻게 분할해 압축하는지를 보여줍니다.이때 전체 그림의 압축 결과는 xxwww bxwxw bbbww xxxww bbbww wwbb가 됩니다.
+//
+//쿼드 트리로 압축된 흑백 그림이 주어졌을 때, 이 그림을 상하로 뒤집은 그림 을 쿼드 트리 압축해서 출력하는 프로그램을 작성하세요.
+
+// 나누면 x 색이전부 흰색이면 w 검은색이면 b
+
+//문자열의 반복자를 인자로 사용
+
+string reverse(string::iterator& it)
+{
+	char head = *it;
+	it++;
+	//문자열의 첫 문자가 b나 w인 경우(사각형의 색의 같은 경우) 문자 그대로 반환
+	if (head == 'b' || head == 'w')
+		return string(1, head);
+
+	//인자로 받은 문자열의 첫 번째 문자를 제외하고 나머지 문자열을 통해 재귀 호출
+	string upperLeft = reverse(it);
+	string upperRight = reverse(it);
+	string lowerLeft = reverse(it);
+	string lowerRight = reverse(it);
+
+	//각각 위와 아래 조각들의 위치를 바꾼다
+	return string("x") + lowerLeft + lowerRight + upperLeft + upperRight;
+}
+
+
+int main(void)
+{
+
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int c;
+	cin >> c;
+
+	while (c--)
+	{
+		string s;
+		cin >> s;
+		string::iterator it = s.begin();
+
+		cout << reverse(it) << "\n";
+
+
+	}
+
+
+	return 0;
+}
